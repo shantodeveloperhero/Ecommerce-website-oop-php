ac3b82b2ee14df87c73f7b4635a90795fcf86293
shantodeveloperhero <shariarshanto78@gmail.com>
2023-03-12 00:20:57 +0600

    first commit

---
+++ shop/success.php
@@ -0,0 +1,42 @@
+<?php include 'inc/header.php'; ?>
+<?php
+  $login = Session::get("cuslogin");
+  if ($login == false) {
+	header("Location:login.php");
+  }
+?>
+<style>
+    .psuccess{width:500px;min-height:200px;text-align:center;border:1px solid #ddd;margin:0 auto;padding:50px;}
+    .psuccess h2{border-bottom:1px solid #ddd;margin-bottom:20px;padding-bottom:10px;}
+   .psuccess p{font-size:18px;line-height:25px;text-align:left;}
+</style>
+ <div class="main">
+    <div class="content">
+    	<div class="section group">
+			<div class="psuccess">
+                <h2>Success</h2>
+                <?php 
+                 $cmrId = Session::get("cmrId");
+                 $amount = $ct->payableAmount($cmrId);
+                 if ($amount) {
+                    $sum = 0;
+                    while ($result = $amount->fetch_assoc()) {
+                        $price = $result['price'];
+                        $sum = $sum+$price;
+                    }
+                 }
+                ?>
+                <p style="color:red">Total Payable Amount(Including Vat) : $
+                <?php
+                  $vat = $sum * 0.1;
+                  $total = $sum+$vat;
+                  echo $total;
+                ?>
+                </p>
+                <p>Thanks For Purchase. Received Your Order Successfully. We will contact you ASAP with delivery details.
+                    Here is your order details....<a href="orderdetails.php">Visit Here..</a></p>
+            </div>
+ 		</div>
+ 	</div>
+</div>
+<?php include 'inc/footer.php'; ?>
